552bc5691428f87e12c24bbf3fef9055875ab056
Alexander Yakovenko <yakovenko.berdyansk@gmail.com>
2025-10-29 08:37:17 +0300

    add views for Mailing

--- mailings/views.py
+++ mailings/views.py
@@ -1,10 +1,30 @@
-from django.contrib.auth.models import User
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Mailing
 
+class MailingListView(ListView):
+    model = Mailing
+    template_name = 'mailings/mailing_list.html'
 
-def home(request):
-    return render(request, "home.html")
+class MailingDetailView(DetailView):
+    model = Mailing
+    template_name = 'mailings/mailing_detail.html'
 
-#class UserListView(ListView):
+class MailingCreateView(CreateView):
+    model = Mailing
+    fields = ['start_time', 'end_time', 'frequency', 'status', 'message', 'clients']
+    template_name = 'mailings/mailing_form.html'
+    success_url = reverse_lazy('mailing_list')
+
+class MailingUpdateView(UpdateView):
+    model = Mailing
+    fields = ['start_time', 'end_time', 'frequency', 'status', 'message', 'clients']
+    template_name = 'mailings/mailing_form.html'
+    success_url = reverse_lazy('mailing_list')
+
+class MailingDeleteView(DeleteView):
+    model = Mailing
+    template_name = 'mailings/mailing_confirm_delete.html'
+    success_url = reverse_lazy('mailing_list')
+
 

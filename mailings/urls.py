5d4c7058dcc4bb2629f1d7217df78b5807f575ff
Alexander Yakovenko <yakovenko.berdyansk@gmail.com>
2025-10-29 08:36:21 +0300

    add urlpatterns for milings

--- mailings/urls.py
+++ mailings/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
+
+from mailings import views
 from mailings.apps import MailingsConfig
-from mailings.views import home
 
 app_name = MailingsConfig.name
 
 urlpatterns = [
-    path('',  home, name='home'),
+    path('mailings/', views.MailingListView.as_view(), name='mailing_list'),
+    path('mailings/<int:pk>/', views.MailingDetailView.as_view(), name='mailing_detail'),
+    path('mailings/create/', views.MailingCreateView.as_view(), name='mailing_create'),
+    path('mailings/<int:pk>/update/', views.MailingUpdateView.as_view(), name='mailing_update'),
+    path('mailings/<int:pk>/delete/', views.MailingDeleteView.as_view(), name='mailing_delete'),
 ]

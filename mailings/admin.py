eed1fb937a76d57f97cfd33c07c3b9a34fbe1958
Alexander Yakovenko <yakovenko.berdyansk@gmail.com>
2025-10-29 08:01:48 +0300

    add admin.py

--- mailings/admin.py
+++ mailings/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Client, Message, Mailing
 
-# Register your models here.
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'email', 'comment')
+    search_fields = ('full_name', 'email')
+    list_filter = ('full_name',)
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('subject',)
+    search_fields = ('subject', 'body')
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'status', 'start_time', 'end_time', 'message_list', 'clients_list')
+    list_filter = ('status',)
+    search_fields = ('message__subject',)
+
+    def message_list(self, obj):
+        return obj.message.subject
+    message_list.short_description = 'Сообщение'
+
+    def clients_list(self, obj):
+        return ", ".join([client.full_name for client in obj.clients.all()])
+    clients_list.short_description = 'Получатели'
+
